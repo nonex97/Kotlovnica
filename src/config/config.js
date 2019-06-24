582eee426e757c5039c470c78bc9890b827cca4f
Neven Grgurić <34512536+nonex97@users.noreply.github.com>
2019-06-24 12:32:48 +0200

    Delete config.js

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-    apiKey: "AIzaSyB7oWjyD0guQbe8BfSdP0JT-B7nC5cc1Jw",
-    authDomain: "kotlovnica-fcb10.firebaseapp.com",
-    databaseURL: "https://kotlovnica-fcb10.firebaseio.com",
-    projectId: "kotlovnica-fcb10",
-    storageBucket: "kotlovnica-fcb10.appspot.com",
-    messagingSenderId: "714900987280",
-    appId: "1:714900987280:web:7bc9211f2ab44d3f"
-  };
